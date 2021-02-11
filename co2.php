5e9f4f18880278be7b02d5237676234cd6273ab1
Pablo Gon <sector7gp@gmail.com>
2021-02-10 23:26:56 -0300

    update

--- co2.php
+++ co2.php
@@ -1,7 +1,7 @@
   <?php
   include_once("db.config.php");
 
-  $results = DB::query("SELECT * FROM sensors WHERE clientId=1 order by id desc limit 2000", $clientId, $deviceId);
+  $results = DB::query("SELECT * FROM sensors WHERE clientId=2 order by id desc limit 10", $clientId, $deviceId);
 //  $results = DB::query("SELECT created, value, name, location, description FROM ((sensors INNER JOIN device ON sensors.deviceId = device.id) INNER JOIN client ON sensors.clientId = client.id) order by sensors.id desc limit 40", $clientId, $deviceId, $description);
 
 //print_r($results);
@@ -12,37 +12,15 @@
    $deviceId[$key] = $result['deviceId'];
    //print_r($deviceId[$key].'<br>');
    
+   
    switch($deviceId[$key]){
-    case "1F15C": 
+    case "257A00": 
     $barColor[$key] = 'red';
-    $description[$key] = 'Puerta-Serv';
-    break;
-    case "23DA2C": 
-    $barColor[$key] = 'blue';
-    $description[$key] = 'Puerta-Estudio';
-    break;
-    case "25922F": 
-    $barColor[$key] = 'yellow';
-    $description[$key] = 'Puerta-Taller';
-    break;
-    case "1EEED8": 
-    $barColor[$key] = 'green';
-    $description[$key] = 'Taller';
-    break;
-    case "24F28": 
-    $barColor[$key] = 'orange';
-    $description[$key] = 'Living';
-    break;
-    case "248DC7": 
-    $barColor[$key] = 'purple';
-    $description[$key] = 'Patio';
-    break;
-    case "257B5D": 
-    $barColor[$key] = 'lightblue';
-    $description[$key] = 'Cocina';
+    $description[$key] = 'DEMO';
     break;
 
   }
+
 }
 $created = json_encode($created);
 $value = json_encode($value);
@@ -64,7 +42,7 @@ $description = json_encode($description);
   }
 </script>
 
-<div id="graficaLineal"></div>
+<div id="co2"></div>
 
 <script type="text/javascript">
   var today = new Date();
@@ -73,7 +51,7 @@ $description = json_encode($description);
   yesterday.setDate(yesterday.getDate() - 1);
   //console.log(yesterday);
 
-  var hourAgo = new Date(today.getTime() - (1000*10*60));
+  var hourAgo = new Date(today.getTime() - (1000*20*60));
   //console.log(hourAgo);
 
   created = crearCadenaLineal('<?php echo $created; ?>');
@@ -83,7 +61,6 @@ $description = json_encode($description);
   description = crearCadenaLineal('<?php echo $description; ?>');
   var data = [
   {
-    //x: unpack(created, 'Date'),
     x: created,
     y: value,
     text: description,
@@ -95,7 +72,7 @@ $description = json_encode($description);
 
 
   var layout = {
-    title: 'Produccion',
+    title: 'Testing',
     //font: {size: 12}
 
     hovermode:'closest',
@@ -105,35 +82,13 @@ $description = json_encode($description);
 
       xaxis: {
         range: [hourAgo, Date.now()],
-        rangeselector: {buttons: [
-          {
-            count: 20,
-            label: '20m',
-            step: 'minute',
-            stepmode: 'backward'
-          },
-          {
-            count: 1,
-            label: '1h',
-            step: 'hour',
-            stepmode: 'backward'
-          },
-          {
-            count: 168,
-            label: '1w',
-            step: 'hour',
-            stepmode: 'backward'
-          },
-          {step: 'all'}
-          ]},
-          type: 'Date',
-          autosize: true
-        }
-      };
+        autosize: false
+      }
+    };
 
   //var data = [trace1, trace2];
 
-  Plotly.newPlot('graficaLineal', data, layout, {responsive: false});
+  Plotly.newPlot('co2', data, layout, {responsive: false});
 
   // setInterval(function() {
   //   Plotly.extendTraces('graficaLineal', { x: [[crearCadenaLineal('<?php echo $created; ?>')]], y: [[crearCadenaLineal('<?php echo $value; ?>')]] }, [0])
--- index.php
+++ index.php
@@ -1,30 +1,32 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Viewer</title>
 	<meta charset="utf-8">
-	<meta http-equiv="refresh" content="20">
+	<meta http-equiv="refresh" content="120">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="libs/jquery-3.5.1.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<script src="libs/plotly-latest.min.js"></script>
+	<link rel="stylesheet" href="custom.css">
 </head>
 
 <body>
-
-
-
-
 	<div class="container">
 		<div clase="row">
 			<div class="col">
 				<div class="panel panel-default">
-					<div class="panel panel-heading"> <?php echo $description; ?> </div>
+					<div class="panel panel-heading"> <?php echo "Prueba de sensores WiFi" ?> </div>
 					<div class="panel panel-body">
 						<div class="row">
 							<div class="col-lg">
 								<div id="cargaLineal"></div>
+								<div id="cargaCO2"></div>
 							</div>
 						</div>
 					</div>
@@ -33,11 +35,38 @@
 		</div>
 	</div>
 </body>
+
+<?php
+
+include_once("db.config.php");
+//echo ">>>>>>>".$_SESSION["lastIP"]."<->".$_SERVER['REMOTE_ADDR'];
+
+if (strcmp($_SESSION["lastIP"],$_SERVER['REMOTE_ADDR'])!==0)
+{
+	echo "saved";
+	$_SESSION["lastIP"] = $_SERVER['REMOTE_ADDR'];
+	DB::insert('access', array(
+		'IP' => $_SERVER['REMOTE_ADDR'],
+		'xF' => $_SERVER['HTTP_X_FORWARDED_FOR']
+	));
+}
+?>
+
+<footer class="bg-light text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">sector7gp.com </a>
+     <!-- - lastIP: <?php echo $_SESSION["lastIP"]." - current: ".$_SERVER['REMOTE_ADDR'];?> -->
+  </div>
+  <!-- Copyright -->
+</footer>
+
 </html>
 
 <script type="text/javascript">
 	$(document).ready(function(){
 		$('#cargaLineal').load('lineal.php');
-		//$('#cargaBarras').load('barras.php');
+		$('#cargaCO2').load('co2.php');
 	});
 </script>
